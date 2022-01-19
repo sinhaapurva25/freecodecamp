85ef5f2143c18049c50d0d5f740e3ead9bbb62d4
Apurva Sinha <apurvasinha@4ctechventures.com>
2022-01-20 01:12:35 +0530

    arithmetic arranger

--- Scientific Computing With Python/boilerplate-arithmetic-formatter-3/arithmetic_arranger.py
+++ Scientific Computing With Python/boilerplate-arithmetic-formatter-3/arithmetic_arranger.py
@@ -1,80 +1,61 @@
-def arithmetic_arranger(problems,*args):
-    space = '    '
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    line4 = ''
-
+def exceptionHandling(problems):
+    message = ''
     if len(problems) > 5:
-        raise Exception('Error: Too many problems')
+        message = 'Error: Too many problems.'
 
     for i in problems:
-        i = i.strip()
-        # print("i.strip()",i)
-        if i.find('*') > 0 or i.find('/') > 0:
-            raise Exception("Error: Operator must be '+' or '-'")
-        for k in i:
-            if ((k >= 'a' and k <= 'z') or (k >= 'A' and k <= 'Z')):
-                raise Exception("Error: Numbers must only contain digits")
-        k = i.find('+') + i.find('-')
+        if i.split()[1]=='/' or i.split()[1]=='*':
+            message = "Error: Operator must be '+' or '-'."
+            break
 
-        element1 = int(i[:k])
-        element2 = int(i[k + 3:])
+    numb = ''
+    for j in [i.split()[0] for i in problems]+[i.split()[2] for i in problems]:
+        numb += j
+    for j in numb:
+        if j not in '0123456789':
+            message = 'Error: Numbers must only contain digits.'
+            break
 
-        if (element1 > 9999) or (element2 > 9999):
-            raise Exception("Error: Numbers cannot be more than four digits")
-        str_element1 = i[:k]
-        str_element2 = i[k + 3:]
+    for j in [i.split()[0] for i in problems]+[i.split()[2] for i in problems]:
+        if int(j) > 9999:
+            message = 'Error: Numbers cannot be more than four digits.'
+            break
+    return message
+def arithmetic_arranger(problems,resBoolean=False):
+    m = exceptionHandling(problems)
+    if m is False:
+        num1Arr = [int(i.split()[0]) for i in problems]
+        operandArr = [i.split()[1] for i in problems]
+        num2Arr = [int(i.split()[2]) for i in problems]
 
-        len_el_1 = len(str_element1)
-        len_el_2 = len(str_element2)
+        space = '    '
+        line1 = ''
+        line2 = ''
+        line3 = ''
+        res = ''
 
-        if (len_el_1 == len_el_2):
-            indent_len = 0
-            line1 = line1 + '  ' + str_element1 + space
-            line2 = line2 + i[k + 1] + ' ' + str_element2 + space
-            dash_length = len(i[k + 1] + ' ' + str_element2)
-            dash = ''
-            for d in range(0, dash_length):
-                dash = dash + '-'
-            line3 = line3 + dash + space
-        elif (len_el_1 > len_el_2):
-            indent_len = (len_el_1 - len_el_2)
-            indent = ''
-            for d in range(0, indent_len):
-                indent = indent + ' '
-            line1 = line1 + '  ' + str_element1 + space
-            line2 = line2 + i[k + 1] + ' ' + indent + str_element2 + space
-            dash_length = len(i[k + 1] + '' + str_element1)
-            dash = ''
-            for d in range(0, dash_length + 1):
-                dash = dash + '-'
-            line3 = line3 + dash + space
-        else:
-            indent_len = (len_el_2 - len_el_1)
-            indent = ''
-            for d in range(0, indent_len):
-                indent = indent + ' '
-            line1 = line1 + '  ' + indent + str_element1 + space
-            line2 = line2 + i[k + 1] + ' ' + str_element2 + space
-            dash_length = len(i[k + 1] + '' + str_element2)
-            dash = ''
-            for d in range(0, dash_length + 1):
-                dash = dash + '-'
-            line3 = line3 + dash + space
+        for idx in range(len(problems)):
+            num1 = str(num1Arr[idx])
+            num2 = str(num2Arr[idx])
+            
+            if len(str(num1Arr[idx])) > len(str(num2Arr[idx])):
+                num2 = ' '*(len(str(num1Arr[idx])) - len(str(num2Arr[idx]))) + str(num2Arr[idx])
+            if len(str(num1Arr[idx])) < len(str(num2Arr[idx])):
+                num1 = ' '*(len(str(num2Arr[idx])) - len(str(num1Arr[idx]))) + str(num1Arr[idx])
 
-        for a in args:
-            if a:
-                if i[k + 1] == '+':
-                    sum = element1 + element2
+            line1 += ' '*2 + num1 + space
+            line2 += operandArr[idx] + ' ' + num2 + space
+            line3 += '-' * len(operandArr[idx] + ' ' + num2) + space
+
+            if resBoolean:
+                sum = 0
+                if operandArr[idx] == '+':
+                    sum = num1Arr[idx] + num2Arr[idx]
                 else:
-                    sum = element1 - element2
-                str_sum = str(sum)
-                indent_len_dash = abs(max(len_el_1, len_el_2) - len(
-                    str_sum)) + 2  # adding one space for operand and another for the pace between the second operant and operand
-                indent = ''
-                for d in range(0, indent_len_dash):
-                    indent = indent + ' '
-                line4 = line4 + indent + str_sum + space
-    arranged_problems = line1 + '\n' + line2 + '\n' + line3 + '\n' + line4
-    return arranged_problems
+                    sum = num1Arr[idx] - num2Arr[idx]
+                res += ' ' * (2 + max(len(str(num1Arr[idx])),len(str(num2Arr[idx]))) - len(str(sum))) + str(sum) + space
+
+        arranged_problems = line1 + '\n' + line2 + '\n' + line3 + '\n' + res
+        return arranged_problems
+    else:
+        return m
