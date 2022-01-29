cac53ec57bf3bbab16b674f7b71b931ef8c6ad98
Apurva Sinha <sinhaapurva25@gmail.com>
2022-01-29 22:36:07 +0530

    Update time_series_visualizer.py

--- Data Analysis With Python/boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
+++ Data Analysis With Python/boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
@@ -5,7 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date',parse_dates=True)#parse_dates=[0] or parse_dates=['date']
+df = pd.read_csv(r'Data Analysis With Python\boilerplate-page-view-time-series-visualizer\fcc-forum-pageviews.csv', index_col='date',parse_dates=True)#parse_dates=[0] or parse_dates=['date']
 
 # Clean data
 df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
