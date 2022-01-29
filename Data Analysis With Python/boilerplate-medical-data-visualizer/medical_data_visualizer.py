1356da9a478fba34167e736044f61cb2691fd64d
Apurva Sinha <sinhaapurva25@gmail.com>
2022-01-29 22:34:24 +0530

    Update medical_data_visualizer.py

--- Data Analysis With Python/boilerplate-medical-data-visualizer/medical_data_visualizer.py
+++ Data Analysis With Python/boilerplate-medical-data-visualizer/medical_data_visualizer.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = pd.read_csv('medical_examination.csv')
+df = pd.read_csv(r'Data Analysis With Python\boilerplate-medical-data-visualizer\medical_examination.csv')
 
 # Add 'overweight' column
 df.loc[(df['weight']/((df['height']/100)*(df['height']/100))) > 25, 'overweight'] = 1
